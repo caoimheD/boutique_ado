9ad799a164fbf0e9853a63096e04dbd550dccc6d
caoimheD <caoimhe.db5@gmail.com>
2023-02-14 13:42:50 +0000

    change debug to dev

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_+6d+#q_p+5vj7_v*q4*dsa8bz9p1-=y)3^t6yz0gak=2u*gg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-p5.herokuapp.com', 'localhost']
 
